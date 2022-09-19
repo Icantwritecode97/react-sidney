e9cf4194e829079dc8f5db8c344986764b1f0995
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-09-19 20:01:32 +0300

    701

---
+++ app-contact-manager/modules/contact.js
@@ -0,0 +1,29 @@
+export const render = (contact) => {
+  const { name, surname, phone, email } = contact;
+  const container = document.createElement('article');
+  container.classList.add('contact', 'border', 'p-3', 'mb-4');
+
+  container.innerHTML = `
+    <h1>${name + ' ' + surname}</h1>
+    <ul>
+      <li>${phone}</li>
+      <li>${email}</li>
+    </ul>
+    <button title="Delete"
+      type="button"
+      class="btn btn-secondary"
+    >Delete</button>
+
+    <button title="Edit"
+      type="button"
+      class="btn btn-secondary mx-2"
+    >Edit</button>
+
+    <button title="Add pet"
+      type="button"
+      class="btn btn-secondary"
+    >Add pet</button>
+  `;
+
+  return container;
+};
--- app-contact-manager/modules/notificationBar.js
+++ app-contact-manager/modules/notificationBar.js
@@ -1,5 +1,7 @@
 const notificationBar = document.querySelector('.notification-bar');
 
+// event delegation pe notificationBar
+
 export const addMessage = (messageElement) => {
   notificationBar.append(messageElement);
 };
